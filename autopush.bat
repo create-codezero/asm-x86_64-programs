805142288b173572b9f8942b7e5cad58f83b1217
create-codezero <2k24.cs1c.2413695@gmail.com>
2025-09-18 19:40:34 +0530

    Auto update 18-09-2025 19:40:34.91

--- autopush.bat
+++ autopush.bat
@@ -2,6 +2,7 @@
 cd /d "E:\mainProject\os-project"
 git add .
 git commit -m "Auto update %date% %time%"
-@REM git push origin main
-git push origin master
+git push origin main
+@REM for first upload comment above line and uncomment below line
+@REM git push origin master
 pause
